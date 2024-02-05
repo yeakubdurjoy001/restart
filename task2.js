b1163b8c577b9c078b99828a97bc7dae740f16ad
YeakubDurjoy001 <your.email@example.com>
2024-02-06 01:22:04 +0600

    added task2

---
+++ task2.js
@@ -0,0 +1,9 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+var totalNumberOfTheSubjects =
+  mathematics + biology + chemistry + physics + bangla;
+var averageNumber = totalNumberOfTheSubjects / 5;
+console.log(averageNumber.toFixed(2));
