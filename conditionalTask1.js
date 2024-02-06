30229147fe931ce7859d6af1e7ee09a825ce348a
YeakubDurjoy001 <your.email@example.com>
2024-02-06 23:16:04 +0600

    added conditional task

---
+++ conditionalTask1.js
@@ -0,0 +1,7 @@
+const burger = 501;
+const coke = 30;
+if (burger > 500) {
+  console.log("you got a free coke!");
+} else {
+  console.log("coke is", coke, "tk");
+}
---
+++ practice.js
@@ -0,0 +1,7 @@
+const salary = 25000;
+const isBCS = false;
+if (salary > 20000 && isBCS == true) {
+  console.log("eso baba kobul");
+} else {
+  console.log("vag tui mokbul");
+}
