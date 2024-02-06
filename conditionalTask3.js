00eec952bda4d4585ce42bc0c15c740eb137dd69
YeakubDurjoy001 <your.email@example.com>
2024-02-07 00:09:53 +0600

    added third task conditional

---
+++ conditionalTask3.js
@@ -0,0 +1,12 @@
+var studentScore = 70;
+if (studentScore >= 90 || studentScore >= 100) {
+  console.log("you got A");
+} else if (studentScore >= 80 || studentScore >= 89) {
+  console.log("you got B");
+} else if (studentScore >= 70 || studentScore >= 79) {
+  console.log("you got C");
+} else if (studentScore >= 60 || studentScore >= 69) {
+  console.log("you got D");
+} else {
+  console.log("you got F");
+}
